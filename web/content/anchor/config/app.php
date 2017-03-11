45e3aceb8f81d3e13df084f60514ecd707fd0aae
Kirsten Berg <kirsten.berg@me.com>
2017-03-11 03:10:14 -0600

    final personal site

---
+++ web/content/anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'url' => '/',
+	'index' => '',
+	'timezone' => 'America/New_York',
+	'key' => '15yD06ETLF5683g6qU6NjHeXsUCRvh4V',
+	'language' => 'en_GB',
+	'encoding' => 'UTF-8'
+);
---
+++ web/content/anchor/config/db.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'default' => 'mysql',
+	'prefix' => 'anchor_',
+	'connections' => array(
+		'mysql' => array(
+			'driver' => 'mysql',
+			'hostname' => '23.253.3.16',
+			'port' => '3306',
+			'username' => '1031023_me',
+			'password' => 'Hercules91!',
+			'database' => '1031023_blog',
+			'charset' => 'utf8'
+		)
+	)
+);
